e693ffd4a101cf091fd4a595d7a00195e52ee5ed
Abd-Alrahman12 <abdalrahman.dev1@gmail.com>
2026-03-13 02:46:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 let offers = [];
 let portfolio = [];
 let requests = [];
